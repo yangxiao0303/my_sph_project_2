eeea332f518edef2d78715ff86b335043827281b
yangxiao <yangxiao0303@gmail.com>
2023-11-10 18:32:30 +0800

    01_static-page-home

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import all the full-scale components
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+
+// import the reset css file
+import '@/assets/css/reset.css'
+
+// asign full-scale components
+Vue.component(Header.name,Header);
+Vue.component(Footer.name,Footer);
+
+
 
 Vue.config.productionTip = false
 
